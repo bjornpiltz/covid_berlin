66319aef83ae4f9b988e420cf5a88a3883a5bc75
bjornpiltz <bjornpiltz@gmail.com>
2020-10-11 14:08:38 +0200

    Update run.py

--- run.py
+++ run.py
@@ -1,1 +1,8 @@
-print("Hello, world!")
+import pandas as pd
+
+# Read Berlin Covid-19 cases  and convert from wide to long format.
+df = pd.read_csv("https://www.berlin.de/lageso/_assets/gesundheit/publikationen/corona/meldedatum_bezirk.csv", sep=';')
+df = df.melt(id_vars="Datum")
+df = df.rename(columns={"Datum": "date", "variable": "location", "value" : "new_cases"})
+df.set_index('date', inplace=True)
+df.to_csv("LAGeSo.csv")
